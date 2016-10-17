aa1cc3e5420ab21f705e72bdfe2c24ce8948e545
Christopher Tham <chriswt91@gmail.com>
2016-10-17 10:37:37 -0700

    Implement API endpoint for location search

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 // API routes configuration
+require('./config/routes.js')(app);
 
 // Serve static files from client
 app.use(express.static(__dirname + '/../client'));
